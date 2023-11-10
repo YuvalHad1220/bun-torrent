a8482dda7ed0c0406deaf9de4c7365debd70cf16
MilkaPeeka <35yuval2@gmail.com>
2023-11-10 09:32:52 +0200

    p

--- frontend/src/Torrent.tsx
+++ frontend/src/Torrent.tsx
@@ -1,8 +1,12 @@
 const Torrent = () => {
     return (
-        <>
-        hello torrent
-        </>
+        <form>
+            <input placeholder="select Client"/>
+            <input placeholder="select upload speed"/>
+            <input placeholder="select download speed"/>
+            <input placeholder="Progress"/>
+            <input type="file" accept=".bittorrent" multiple className="file-input file-input-bordered file-input-primary w-full max-w-xs" />
+        </form>
     )
 };
 
