b9eedcc1961791714953070e76377191de163342
MilkaPeeka <35yuval2@gmail.com>
2023-11-11 12:13:13 +0200

    push

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,20 @@
 import TorrentForm from "./TorrentForm.tsx";
 import ClientForm from "./ClientForm.tsx";
+import { useState } from "react";
 
 function App() {
+  const [isDisplayTorrent, setIsDisplayTorrent] = useState(true);
 
   return (
-    <>
-      <ClientForm />
-      <TorrentForm />
-    </>
+    <div className="grid grid-cols-5 h-screen gap-3 p-3">
+      <div className="col-span-4 rounded-2xl shadow-2xl bg-base-300 h-full p-3">
+        {isDisplayTorrent ? <TorrentForm /> : <ClientForm />}
+      </div>
+      <div className="col-span-1 rounded-2xl shadow-2xl bg-base-300 h-full flex flex-col gap-3 p-2">
+        <button onClick={() => setIsDisplayTorrent(true)} className={`btn rounded-2xl hover:bg-primary-focus ${isDisplayTorrent ? "btn-primary" : ""}`}>Torrents</button>
+        <button onClick={() => setIsDisplayTorrent(false)} className={`btn rounded-2xl hover:bg-primary-focus ${!isDisplayTorrent ? "btn-primary" : ""}`}>Clients</button>
+      </div>
+    </div>
 
   )
 }
